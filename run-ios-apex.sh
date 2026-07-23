7c144eb9ddbb6e1ce642c56a5a4623d605fd8858
Desh Raj <desh@phantixai.com>
2026-07-23 10:50:55 +0530

    upgrade : android policy

--- run-ios-apex.sh
+++ run-ios-apex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run the APEX app on iOS Simulator
 echo "$(pwd)/.env.iitians" > /tmp/envfile
-#yarn react-native run-ios --scheme APEX --simulator "iPhone 17"
-yarn react-native run-ios --scheme APEX --device "Desh iPhone"
+yarn react-native run-ios --scheme APEX --simulator "iPhone 17"
+#yarn react-native run-ios --scheme APEX --device "Desh iPhone"
